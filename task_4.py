d32b39b7d35d3171897f31080d35d1199cb0f57a
nastyyua <127897258+nastyyua@users.noreply.github.com>
2023-03-25 17:41:13 +0000

    задания синтаксиса

---
+++ task_4.py
@@ -0,0 +1,16 @@
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+c = a[1]
+f = b[3]
+print(c+f)
+c = a[2]
+f = b[2]
+print(c+f)
+c = a[0]
+f = b[0]
+print(c*f)
+c = a[1]
+f = b[0]
+print(c**f)
+c = a+b
+print(c)
