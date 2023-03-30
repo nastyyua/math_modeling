ded4e2895c9f2d72292a066db6e7652fff398b41
Alexey <48138804+ABaigashov@users.noreply.github.com>
2023-03-30 09:45:55 +0300

    Более удобный вариант решения

--- task_6.py
+++ task_6.py
@@ -1,3 +1,9 @@
 i = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for b in range(1, 10, 1):
     print(i[0]*b, i[1]*b, i[2]*b, i[3]*b, i[4]*b, i[5]*b, i[6]*b, i[7]*b, i[8]*b)
+    
+# Попробуйте так
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(f' {i*j} ', end='')
+    print()
