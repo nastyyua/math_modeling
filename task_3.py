8752cf8fccd7f594803553f27cbaf82b78a3abbd
nastyyua <127897258+nastyyua@users.noreply.github.com>
2023-05-08 16:10:46 +0000

    функции

--- task_3.py
+++ task_3.py
@@ -1,10 +1,12 @@
+from fizical_constant import g 
+
 m = 5
 h = 10
 V = 2
-g = 10
 
-
-def mehanic(*args):
+def mehanic(m=1, V=1, h=1):
     E = (m*V**2)/2 + m*g*h
     print(E)
     return E
+
+print(mehanic(5,2,10))
